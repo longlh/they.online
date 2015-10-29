761559235ce9c2d58914dd4d2eee356f717a3889
HoangNong <hoang.nong@ntq-solution.com.vn>
2015-10-29 23:58:13 +0700

    Done logout + Remove account folder

--- src/management/_shared/partials/header.js
+++ src/management/_shared/partials/header.js
@@ -1,12 +1,12 @@
 'use strict';
 
 require('..').controller('shared.controllers.header', [
+	'$scope',
 	'ENV',
 	'shared.services.storage',
-	function(ENV, storage) {
+	function($scope, ENV, storage) {
 		this.signOutUrl = ENV.signOutUrl;
 
 		this.session = storage.get('session');
-		console.log(this.session);
 	}
 ]);
--- src/management/account/partials/account.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-require('..').controller('account.controller.account', [
-	'$scope',
-	function($scope) {
-		console.log('My account');
-	}
-]);
